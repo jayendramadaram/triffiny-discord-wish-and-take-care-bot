cee674499789f24d8f207bc289aa2ce8ab740693
jayendra_madara <75383012+jayendramadaram@users.noreply.github.com>
2021-09-10 19:49:00 +0000

    update strategy,activity status,dp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlite3
 import os
 import discord
-from subprocess import run
 
 import asyncio
 import datetime as dt
@@ -11,6 +10,7 @@ from keep_alive import keep_alive
 from discord.ext import tasks
 import asyncio
 from replit import db
+import json
 
 
 
@@ -24,6 +24,8 @@ flag = 0
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    activity = discord.Game(name="with jay")
+    await client.change_presence(status=discord.Status.offline, activity=activity)
     msg1.start()
 
 
@@ -31,7 +33,8 @@ async def on_ready():
 async def msg1():
     message_channel = client.get_channel(884854235441791086)
     num = db["day"]
-    await message_channel.send("hey honey you have only `{}` days to achevie your dreams here are your tasks".format(num))
+    myid = '<@763029387674124288>'
+    await message_channel.send("%s honey you have only `{}` days to achevie your dreams here are your tasks".format(num) % myid)
     con = sqlite3.connect('mydatabase.db')
     rows = sql_fetch(con)
     for i in rows:
@@ -39,19 +42,27 @@ async def msg1():
     con.close()
 
     await message_channel.send("-----------------------------------------------------------------------")
-    
-    await message_channel.send("-----------------------------------------------------------------------")
+    if db['strat'] != "":
+      await message_channel.send(db['strat'])
+      await message_channel.send("-----------------------------------------------------------------------")
     db["day"] = int(db["day"]) - 1
+    with open(r'tasks.json' , "r") as js:
+      data = json.loads(js.read())
+      for i in range(len(data)):
+        if int(data[i]["dueDate"].split("-")[2]) < int(dt.datetime.now().strftime("%d")):
+          await message_channel.send("pending schedule `{0}` on `{1}`".format(data[i]['task'] ,data[i]['dueDate'] ))
+        elif int(data[i]["dueDate"].split("-")[2]) == int(dt.datetime.now().strftime("%d")):
+          await message_channel.send("```todays schedule {}```".format(data[i]['task']))
+          break
+
 
 
 @msg1.before_loop
 async def before_msg1():
     for _ in range(60*60*24):
-      if dt.datetime.now().hour == 9:  
+      if dt.datetime.now().hour == 1:  
             return
       await asyncio.sleep(1)
-
-
 def sql_note(entities , con):
     cursorObj = con.cursor()
 
@@ -59,6 +70,19 @@ def sql_note(entities , con):
     cursorObj.execute('INSERT INTO note( name) VALUES( ?)', [entities])
     
     con.commit()
+
+def task_com(task):
+  with open(r'tasks.json' , "r") as js:
+    data = json.loads(js.read())
+    for i in range(len(data)):
+      if data[i]['dueDate'].split("-")[2] == task:
+        data.pop(i)
+        break
+    with open("tasks.json" , "w") as dat:
+      dat.write(
+      json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+  
+  
 
 def sql_nots(con):
 
@@ -105,18 +129,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('##'):
+    elif message.content.startswith('##'):
       await message.reply("yo i am with ya")
 
+    elif message.content.strip() == "help":
+      await message.channel.send("""
+      ```
+      ## --> to check uptime of bot\n
+      ! + randomize/random  --> to random choice\n
+      < content | date end | time will be taken > --> task add to sqlite3\n
+      < content_name > --> delete task from sqlite3\n
+      input ==sentence with **change && dp** --> changes profile pic\n
+      note: content --> adds to notice list\n
+      strategy: content  --> adds stretegy\n
+      complete! content  --> deletes from JSON
+      ```
+      """)
 
     #PUBLIC SEARCH
-    if message.content.startswith('!'):
+    elif message.content.startswith('!'):
         print("REQUEST:", message.content,'> requested by {}'.format(message.author.name))
         content = message.content.strip().split()
         if len(content) >= 1 :
-            # print(1)
             if 'random' in content or 'randomize' in content and flag==0:
-                
                 await message.reply('cool i will randomize dude tell me what are your picks')
                 flag = 1
         else:
@@ -124,20 +159,6 @@ async def on_message(message):
     elif flag==1:
         await message.reply(random.choice(message.content.strip().split()))
         flag=0
-    elif message.content.startswith('$'):
-        con = sqlite3.connect('mydatabase.db')
-        if "show" in message.content.split() and "tasks" in message.content.split():
-            rows = sql_fetch(con)
-            for i in rows:
-                await message.channel.send(i)
-            await message.reply("yo reached bottom honey")
-            con.close()
-        elif "add" in message.content.split():
-            await message.reply('umm hmm alright bud i will insert tell me your tasks in < TASK | DATE_START | DATE_END  > format is must make about spaces and symbols')
-            con.close()
-        elif "delete" or "done" in message.content.split():
-            await message.reply('Congrats honey your task log is becomming short ')
-            con.close()
     elif message.content.startswith('<') and message.content.endswith('>'):
         con = sqlite3.connect('mydatabase.db')
         msg = message.content.split()
@@ -172,22 +193,39 @@ async def on_message(message):
       con = sqlite3.connect('mydatabase.db')
       sql_note(entities , con)
       con.close()
-    elif "notes" in message.content and "show" in message.content:
-      con = sqlite3.connect('mydatabase.db')
-      for i in sql_nots(con):
-        await message.channel.send(i)
-      await message.reply("this is your current list")
-      con.close()
     elif message.content.startswith("strategy:"):
       entities =  str(message.content.split(":")[1])
       db["strat"] = entities
-    elif "strat" in message.content and "show" in message.content:
-      con = sqlite3.connect('mydatabase.db')
-      await  message.channel.send("strat")
-      con.close()
+    elif message.content.startswith("completed!"):
+      task = message.content.split("!")[1]
+      task_com(task)
+    elif "show" in  message.content:
+      if "schedule" in message.content:
+        with open(r'tasks.json' , "r") as js:
+          data = json.loads(js.read())
+          for i in data:
+            await message.channel.send("```you have task:`{0}` on {1}```".format(i["task"],i["dueDate"]))
+      elif "strat" in message.content:
+        con = sqlite3.connect('mydatabase.db')
+        await  message.channel.send(db["strat"])
+        con.close()
+      elif "notes" in message.content :
+        con = sqlite3.connect('mydatabase.db')
+        for i in sql_nots(con):
+          await message.channel.send(i)
+        await message.reply("this is your current list")
+        con.close()
+      elif  "tasks" in message.content.split():
+        con = sqlite3.connect('mydatabase.db')
+        rows = sql_fetch(con)
+        for i in rows:
+            await message.channel.send(i)
+        await message.reply("yo reached bottom honey")
+        con.close()
+      
+      
     
 
 
 keep_alive()
 client.run(my_secret)
-
